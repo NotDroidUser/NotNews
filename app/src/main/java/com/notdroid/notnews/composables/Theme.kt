c30d766aae2468bc749f661ad3bc9b442d5bf832
NotDroidUser <r1d1p1j1@gmail.com>
2026-01-13 17:56:26 -0500

    added font alegreya, and some drawables

--- app/src/main/java/com/notdroid/notnews/composables/Theme.kt
+++ app/src/main/java/com/notdroid/notnews/composables/Theme.kt
@@ -11,6 +11,9 @@ import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import com.notdroid.notnews.R
 
 val lightNotNewsTheme= lightColorScheme(
   primary = Color(0xFF2196F3),
@@ -21,6 +24,29 @@ val darkNotNewsApp= darkColorScheme(
   primary = Color(0xFF0D47A1),
   secondary = Color(0xFF1976D2)
 )
+
+val alegreya=FontFamily(fonts = listOf(
+  Font(R.font.alegreya_regular),
+  Font(R.font.alegreya_black),
+  Font(R.font.alegreya_black_italic),
+  Font(R.font.alegreya_bold),
+  Font(R.font.alegreya_bold_italic),
+  Font(R.font.alegreya_extra_bold),
+  Font(R.font.alegreya_extra_bold_italic),
+  Font(R.font.alegreya_italic),
+  Font(R.font.alegreya_medium),
+  Font(R.font.alegreya_medium_italic),
+  Font(R.font.alegreya_sc_black),
+  Font(R.font.alegreya_sc_black_italic),
+  Font(R.font.alegreya_sc_bold),
+  Font(R.font.alegreya_sc_bold_italic),
+  Font(R.font.alegreya_sc_extra_bold),
+  Font(R.font.alegreya_sc_extra_bold_italic),
+  Font(R.font.alegreya_sc_italic),
+  Font(R.font.alegreya_sc_medium),
+  Font(R.font.alegreya_sc_medium_italic),
+  Font(R.font.alegreya_sc_regular),
+))
 
 
 
